451c1c6d2a21bcae98aab2428d2d5e20684924b6
Christian <115067566+kitearuba@users.noreply.github.com>
2023-05-12 13:27:32 +0200

    Update Primitiva.java
    Agregar valores del ticket a comprobar en Arguments o no funcionara el codigo

--- Primitiva.java
+++ Primitiva.java
@@ -1,6 +1,3 @@
-package primitiva;
-
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.Random;
 import java.util.ArrayList;
@@ -30,49 +27,30 @@ public class Primitiva {
 
         // Llama a el metodo que extrae valores en argumments y los agregar a ArrayList.
         extracted(args, jugado);
+
+        // Agregar valor de ArrayList jugado a otro array lista para
+        // usar para comparar numeros despues.
+        // No es necesario por preferi hacerlo asi.
         numerosJugados = jugado;
 
-        // Agregar reintegro jugado a ArrayList Reintegro
+        // Agregar reintegro jugado a ArrayList Reintegro. Para comparar separado de los otros numeros.
         reintegroJugado = numerosJugados.get(7);
-        // Eliminar numero de reintegro de ArrayList numerosJugados
+
+        // Eliminar numero de reintegro de ArrayList numerosJugados.
+        // Esto para comparar solo los numeros sin el reintegro y poder ordenar los numeros.
         numerosJugados.remove(7);
+
         // Ordenar numeros de < a >
         Collections.sort(numerosJugados);
 
-        System.out.println("Escoje cuantos sorteos anterior a compropbar tus numeros(elejir entre 1 a 1000: ");
+        System.out.println("Introduce el valor de la cantidad de sorteos a comparar con tu ticket (elejir entre 1 a 6): ");
+        // Scanner para obtener el valor de la cantidad de sorteos que hay que comparar el numero
+        // ingresado por arguments.
         comprobarSorteo=scanner.nextInt();
+        // Metodo principal donde se verifica si el numero introducido en el scanner es valido.
+        // Si lo es entra en el buckle y general la cantidad de boltos indicados, los compara, y imprime su resultado.
+        extracted(random, comprobarSorteo, reintegroJugado, ganador, numerosGanador, numerosJugados);
 
-        if (comprobarSorteo>0 && comprobarSorteo<1001){
-
-            for(int i=0; i<comprobarSorteo;i++){
-                numerosGanador.removeAll(numerosGanador);
-                // Llama a el metodo que usa For loop para generar numeros aleatorios
-                extracted(random, ganador, numerosGanador);
-
-                // Agregar reintegro ganador a ArrayList reintegroGanador
-                reintegroGanador = ganador.get(7);
-
-                // Comparar los numeros jugados y copiar los numeros acertados
-                compareNumeros = new ArrayList<>(numerosGanador.stream().filter(numerosJugados::contains).collect(Collectors.toList()));
-
-                // Boolean para ver si los reintegros son iguales.
-                compareReintegro = reintegroGanador==reintegroJugado;
-
-                // Llama a el metodo para Comprovar si el reintegro es ganador o no.
-                resultadoReintegro = getString(reintegroJugado, compareReintegro);
-                // Llama a el metodo para Comprovar si hay numeros ganadores.
-                resultadoNumeros = getString(numerosJugados, compareNumeros);
-
-
-                System.out.println("\n"+resultadoNumeros+"\n"+resultadoReintegro+
-                        "\n\nNumeros jugado:    "+numerosJugados+"\n Reintegro jugado:  "+reintegroJugado+
-
-                        "\nNumeros ganadores: "+numerosGanador+"\n Reintegro ganador: "+reintegroGanador);
-            }
-        }
-        else{
-            System.out.println("Introduce un valor valido!!!");
-        }
 
     }
 
@@ -81,7 +59,49 @@ public class Primitiva {
             jugado.add(Integer.valueOf(arg));
         }
     }
+    private static void extracted(Random random, int comprobarSorteo, int reintegroJugado, ArrayList<Integer> ganador, ArrayList<Integer> numerosGanador, ArrayList<Integer> numerosJugados) {
+        boolean compareReintegro;
+        String resultadoReintegro;
+        ArrayList<Integer> compareNumeros;
+        String resultadoNumeros;
+        int reintegroGanador;
+        if (comprobarSorteo >0 && comprobarSorteo <7){
+            for(int n = 0 ; n < comprobarSorteo; n++){
+                int z = 1+n;
+                // Vacia la ArrayList de numeros random para cada vuelta del buckle.
+                numerosGanador.removeAll(numerosGanador);
+                // Llama a el metodo que usa For loop para generar numeros aleatorios.
+                extracted(random, ganador, numerosGanador);
 
+                // Agregar reintegro ganador a ArrayList reintegroGanador.
+                reintegroGanador = ganador.get(7);
+
+                // Comparar los numeros jugados y copiar los numeros acertados.
+                compareNumeros = new ArrayList<>(numerosGanador.stream().filter(numerosJugados::contains).collect(Collectors.toList()));
+
+                // Boolean para ver si los reintegros son iguales.
+                compareReintegro = reintegroGanador== reintegroJugado;
+
+                // Llama a el metodo para Comprovar si el reintegro es ganador o no.
+                resultadoReintegro = getString(reintegroJugado, compareReintegro);
+                // Llama a el metodo para Comprovar si hay numeros ganadores.
+                resultadoNumeros = getString(numerosJugados, compareNumeros);
+
+
+
+                System.out.println("\n*****************************\n*****************************\n" +
+                        "\nSORTEO NUMERO: "+ z +"\n"+resultadoNumeros+"\n"+resultadoReintegro+
+                        "\n\nNumeros jugado:    "+ numerosJugados +"\n Reintegro jugado:  "+ reintegroJugado +
+                        "\nNumeros ganadores: "+ numerosGanador +"\n Reintegro ganador: "+reintegroGanador);
+            }
+        }
+        else{
+            System.out.println("Introduce un valor valido!!!");
+        }
+    }
+
+
+    // Metodo para generar numeros aleatorio.
     private static void extracted(Random random, ArrayList<Integer> ganador, ArrayList<Integer> numerosGanador) {
         int y;
         int x;
@@ -107,6 +127,7 @@ public class Primitiva {
         }
     }
 
+    // Metodo para comparar los numeros y dar resultados.
     private static String getString(ArrayList<Integer> numerosJugados, ArrayList<Integer> compareNumeros) {
         String resultadoNumeros;
         if(compareNumeros.size() == 7){
@@ -119,6 +140,7 @@ public class Primitiva {
         return resultadoNumeros;
     }
 
+    // Metodo para comparar los reintegros y dar resultados.
     private static String getString(int reintegroJugado, boolean compareReintegro) {
         String resultadoReintegro;
         if(compareReintegro == true){
